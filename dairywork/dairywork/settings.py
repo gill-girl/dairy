9d10c8e3414c77ac19de81a24b3c54bffad1ebcf
gill-girl <manpritgill326@gmail.com>
2026-04-30 11:42:05 +0530

    update the file with changes

--- dairywork/dairywork/settings.py
+++ dairywork/dairywork/settings.py
@@ -91,6 +91,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
+
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
